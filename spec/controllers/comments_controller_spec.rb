bb75ef96c06e69407e2acd6f8e606a0ef9b9d48f
Megan Swanby <Megan.Swanby@gmail.com>
2016-05-26 10:34:56 -0400

    Add initial rspec tests for comments controller

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe CommentsController do
+  let!(:comment) { Comment.create!(text: "Comment", user: 1, review: 1) }
+
+  describe "GET #new" do
+    it 'assigns the new comment as @comment' do
+      get 'new'
+      expect(assigns(:comment)).to eq(comment)
+    end
+
+    it 'assigns the reviewer_id'
+  end
+
+   describe "GET #show" do
+    it "assigns the requested comment as @comment" do
+      get :show, { id: comment.id }
+      expect(assigns(:comment)).to eq(comment)
+    end
+  end
+
+
+end
