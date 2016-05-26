f97d326cc298c142c0a3ed5eecd857883b9a84a9
Megan Swanby <Megan.Swanby@gmail.com>
2016-05-26 12:28:20 -0400

    Add comments controller tests, no errors

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,20 +1,34 @@
 require "rails_helper"
 
 describe CommentsController do
-  let!(:comment) { Comment.create!(text: "Comment", user: 1, review: 1) }
+  let!(:comment) { Comment.create!(body: "Comment", user_id: 1, review_id: 1) }
 
   describe "GET #new" do
     it 'assigns the new comment as @comment' do
       get 'new'
-      expect(assigns(:comment)).to eq(comment)
-    end
-
-   describe "GET #show" do
-    it "assigns the requested comment as @comment" do
-      get :show, { id: comment.id }
-      expect(assigns(:comment)).to eq(comment)
+      expect(assigns(:comment)).to be_a_new(Comment)
     end
   end
 
+  describe "POST #create" do
+    context "when valid params are passed" do
+      it "creates a new comment as @comment" do
+        post 'create', {comment: {id: comment.id }}
+        expect(assigns(:comment)).to be_an_instance_of(Comment)
+      end
+    end
+
+    context "when invalid params are passed" do
+      it "assigns a newly created but unsaved comment as @comment" do
+        post 'create', {comment: {id: comment.id}}
+        expect(assigns(:comment)).to_not be_valid
+      end
+
+      it "re-renders the 'new' template" do
+        post 'create', {comment: {id: comment.id}}
+        response.should render_template(:new)
+      end
+    end
+  end
 
 end
